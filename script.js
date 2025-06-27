6e595f1c54bb94f6233599c7a13f94022a2674a7
Asya <119005890+asya-space@users.noreply.github.com>
2025-06-27 20:57:03 +0700

    refactor: add original script

---
+++ script.js
@@ -0,0 +1,74 @@
+const morseTable = {
+    '.-':     'a',
+    '-...':   'b',
+    '-.-.':   'c',
+    '-..':    'd',
+    '.':      'e',
+    '..-.':   'f',
+    '--.':    'g',
+    '....':   'h',
+    '..':     'i',
+    '.---':   'j',
+    '-.-':    'k',
+    '.-..':   'l',
+    '--':     'm',
+    '-.':     'n',
+    '---':    'o',
+    '.--.':   'p',
+    '--.-':   'q',
+    '.-.':    'r',
+    '...':    's',
+    '-':      't',
+    '..-':    'u',
+    '...-':   'v',
+    '.--':    'w',
+    '-..-':   'x',
+    '-.--':   'y',
+    '--..':   'z',
+    '.----':  '1',
+    '..---':  '2',
+    '...--':  '3',
+    '....-':  '4',
+    '.....':  '5',
+    '-....':  '6',
+    '--...':  '7',
+    '---..':  '8',
+    '----.':  '9',
+    '-----':  '0',
+};
+
+function encodeStr(str) {
+    let newStr = str.toLowerCase().replace(/\s/g, "**********").split(''),
+      convertStr = [],
+       objKeys = Object.keys(morseTable),
+       objValues = Object.values(morseTable);
+  newStr.forEach((item) => {
+    if(item === "*") {
+      convertStr.push(item)
+    }
+    objValues.forEach((letter, pos) => {
+    if(letter === item) {
+      convertStr.push(objKeys[pos]);
+    }})
+  });
+  let endArr = [];
+  for(let symbols of convertStr) {
+      symbols = symbols.replace(/[-]/gi, 11).replace(/[.]/gi, 10)
+    if(symbols.length < 10 && symbols !== "*") {
+      let count = symbols.length;
+      let zero = "0"
+      while(count < 10) {
+        symbols = zero + symbols;
+        count++;
+      }
+    }
+      endArr.push(symbols);
+  }
+  return endArr.join('');
+}
+
+function encode() {
+      const input = document.getElementById("inputText").value;
+      const output = encodeStr(input);
+      document.getElementById("output").textContent = output || '(empty)';
+    }
