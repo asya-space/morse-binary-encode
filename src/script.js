ee76212364dee2e8a0f7a04315c39c61ab0fae77
Asya Lebedeva <star@asyaspace.com>
2025-07-01 14:06:17 +0700

    build: add script for decoder string

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ const morseTable = {
     '-----':  '0',
 };
 
-function encodeStr(str) {
+function binaryEncoder(str) {
     let newStr = str.toLowerCase().replace(/\s/g, "**********").split(''),
         convertStr = [],
         objKeys = Object.keys(morseTable),
@@ -69,7 +69,7 @@ function encodeStr(str) {
 
 function encode() {
     const input = document.getElementById("inputText").value;
-    const output = encodeStr(input);
+    const output = binaryEncoder(input);
     document.getElementById("output").textContent = output || '(empty)';
     }
 
@@ -78,6 +78,42 @@ document.addEventListener("DOMContentLoaded", () => {
   input.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
       encode();
+    }
+  });
+});
+
+/* decode */
+
+function binaryDecoder(expr) {
+    let splitByBlock = [],
+    letters = [];
+    for (let i = 0; i < expr.length; i += 10) {
+        splitByBlock.push(expr.slice(i, i + 10));
+    }
+    splitByBlock.map((item) => {
+        let char = '';
+        if(item === "**********") char += " ";
+        for(let j = 0; j < 10; j += 2) {
+            const pair = item.slice(j, j + 2)
+            if(pair === "11") char += "-";
+            if(pair === "10") char += ".";
+        }
+        letters.push(morseTable[char] || ' ');
+    })
+    return letters.join("");
+}
+
+function decode() {
+    const input = document.getElementById("enterCode").value;
+    const output = binaryDecoder(input);
+    document.getElementById("showText").textContent = output || '(empty)';
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("enterCode");
+  input.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      decode();
     }
   });
 });
